ae1bb344163f34cf0fcc83f076999f1d656f7442
Sachin M. Rathod <144583341+iSachin22@users.noreply.github.com>
2026-09-11 11:57:26 +0530

    Initial commit: Employee Performance and Appraisal Management System (Spring Boot + Hibernate + MySQL + JWT)

---
+++ src/main/java/com/sachin/appraisal/repository/KpiScoreRepository.java
@@ -0,0 +1,7 @@
+package com.sachin.appraisal.repository;
+
+import com.sachin.appraisal.entity.KpiScore;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KpiScoreRepository extends JpaRepository<KpiScore, Long> {
+}
