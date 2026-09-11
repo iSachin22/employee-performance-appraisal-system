bd2fdffa9ba270192ad5121c9f70b1513b388586
Sachin M. Rathod <144583341+iSachin22@users.noreply.github.com>
2026-09-11 11:57:25 +0530

    Initial commit: Employee Performance and Appraisal Management System (Spring Boot + Hibernate + MySQL + JWT)

---
+++ src/main/java/com/sachin/appraisal/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.sachin.appraisal.repository;
+
+import com.sachin.appraisal.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByEmail(String email);
+}
