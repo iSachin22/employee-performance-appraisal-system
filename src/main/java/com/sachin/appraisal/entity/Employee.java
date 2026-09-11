fbd7314a30da0311ffbd70eb1400052abe672b5a
Sachin M. Rathod <144583341+iSachin22@users.noreply.github.com>
2026-09-11 11:57:22 +0530

    Initial commit: Employee Performance and Appraisal Management System (Spring Boot + Hibernate + MySQL + JWT)

---
+++ src/main/java/com/sachin/appraisal/entity/Employee.java
@@ -0,0 +1,37 @@
+package com.sachin.appraisal.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @ManyToOne
+    @JoinColumn(name = "manager_id")
+    private Employee manager;
+}
